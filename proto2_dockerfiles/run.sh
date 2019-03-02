236601b81810828e99ff51b7e1d856f3aa1bc54a
Spritaro <k5dot.feb@gmail.com>
2019-03-02 20:24:32 +0900

    fix bug

--- proto2_dockerfiles/run.sh
+++ proto2_dockerfiles/run.sh
@@ -5,7 +5,7 @@ docker run \
     --rm \
     --privileged \
     -v /home/pi/.ros/:/root/.ros/ \
-    -v /home/catkin_ws/:/root/catkin_ws/ \
+    -v /home/pi/catkin_ws/:/root/catkin_ws/ \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
