7990fb26048888b4f8d58364c68970200bbd4941
Spritaro <k5dot.feb@gmail.com>
2019-04-29 19:17:50 +0900

    change back to librealsense 2.19.2, with patch to ignore hids

--- proto2_dockerfiles/run.sh
+++ proto2_dockerfiles/run.sh
@@ -9,7 +9,7 @@ docker run \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    ros-cpp-2.21.0-2.2.3 \
+    ros-cpp-2.19.2-2.2.3 \
     bash
     #--net host \
     #-e DISPLAY="DISPLAY" \
