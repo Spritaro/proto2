210de8086a6622cbf28f3e56fcb369b35cc19dea
Spritaro <k5dot.feb@gmail.com>
2019-08-05 09:43:29 +0900

    add roslaunch script

---
+++ proto2_dockerfiles/no_it_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+    --rm \
+    --privileged \
+    -v /home/pi/.ros/:/root/.ros/ \
+    -v /home/pi/catkin_ws/:/root/catkin_ws/ \
+    -p 9090:9090 \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    ros-cpp-2.22.0-2.2.6
